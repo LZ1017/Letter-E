1f289a1e529c48fdc19a8858683d07825430208b
LZ1017 <lillianzhu829@gmail.com>
2021-03-19 00:32:28 -0400

    day1

---  Day 1/e_sketch.js
+++  Day 1/e_sketch.js
@@ -2,4 +2,26 @@ function setup(){
 	var c = createCanvas(1000,1000);
 	background(255)
 
+
+
 }
+
+function draw(){
+	strokeWeight(100);
+	strokeCap(ROUND);
+
+		line(200,200,200,800);
+	for (var i = 200;i<900;i+=300){
+		line(200,i,800,i);
+	}
+	// strokeCap(ROUND);
+	// line(200,200,800,200); //line (x1,y1,x2,y2)
+	// strokeCap(Round);
+	// line(200,400,600,200)；
+	// strokeCap(Round);
+	// line(200,200,200,800);
+}
+
+// function mousePressed(){
+// 	saveCanvas("sketch_01","png")
+// }
