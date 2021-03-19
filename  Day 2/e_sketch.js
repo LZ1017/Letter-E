5ee61cf00a1bd0f585e2de83137e71b0768d7b02
LZ1017 <lillianzhu829@gmail.com>
2021-03-19 17:28:55 -0400

    Update e_sketch.js

---
+++  Day 2/e_sketch.js
@@ -0,0 +1,19 @@
+function setup(){
+	var c = createCanvas(1000,1000);
+	background(255);
+}
+
+function draw(){
+
+	strokeWeight(100);
+	strokeCap(ROUND);
+
+	line(200,200,200,800);
+	line(200,200,700,200);
+
+
+}
+
+// function mousePressed(){
+// 	saveCanvas("sketch_01","png")
+// }
