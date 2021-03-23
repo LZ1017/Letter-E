9b727d400cd9a010ab6087a765be73afb65bf6bd
LZ1017 <lillianzhu829@gmail.com>
2021-03-22 22:48:50 -0400

    day 9

---
+++ Day 9/e_sketch.js
@@ -0,0 +1,29 @@
+let font;
+
+function preload(){
+	font = loadFont("Bubblegum.ttf");
+}
+
+function setup() {
+  createCanvas(1000, 1000);
+
+  background(0);
+  fill(255);
+  noStroke();
+  
+  textAlign(CENTER,CENTER);
+  textFont(font, 1000);
+  text('E', 500,450);
+
+}
+
+function mouseMoved() {
+  fill(random(255));
+  let s = random(50);
+    ellipse(mouseX,mouseY,s,s);
+  }
+  
+
+function mousePressed(){
+	saveCanvas("sketch_01","png")
+}
