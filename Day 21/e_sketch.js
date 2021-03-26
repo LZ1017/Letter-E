71e782c9490af1b2a723ff5a9be1f95d80006c43
LZ1017 <lillianzhu829@gmail.com>
2021-03-26 04:14:19 -0400

    day 21

---
+++ Day 21/e_sketch.js
@@ -0,0 +1,76 @@
+
+let graphic
+let font
+let canvas
+
+function preload() {
+  font = loadFont('Bubblegum.ttf')
+}
+
+function setup() {
+  createCanvas(1000, 1000)
+  background(255)
+  
+  // create offscreen graphics buffer
+  graphic = createGraphics(width, height)
+  
+  // type setup offscreen in buffer
+  graphic.textFont(font)
+  graphic.textAlign(CENTER, CENTER)
+  graphic.blendMode(SCREEN)
+
+  graphic.textSize(1000)
+  graphic.fill('rgba(255, 0, 0, 0.9)')
+  graphic.text('E', width / 2, height / 2)
+
+  graphic.textSize(950)
+  graphic.fill('rgba(0, 255, 0, 0.9)')
+  graphic.text('E', width / 1.9, height / 1.9)
+
+  graphic.textSize(900)
+  graphic.fill('rgba(0, 0, 255, 0.9)')
+  graphic.text('E', width / 2.1, height / 2.1)
+}
+
+const loopDuration = 3 * 100
+
+function draw() {
+  
+  let currentFrame = frameCount % loopDuration
+  let t = currentFrame / loopDuration
+  let u = map(t, 0, 1, 0, 2 * PI)
+
+  background(0)
+  
+  const tiles = 30
+  const tileSize = width / tiles
+  
+  for (let x = 0; x < tiles; x++) {
+    for (let y = 0; y < tiles; y++) {
+      
+      const distortionX = cos(u + x * 0.5) * 20
+      const distortionY = sin(u + y * 0.5) * 20
+      
+      const sx = x * tileSize + distortionX
+      const sy = y * tileSize + distortionY
+      const sw = tileSize + distortionX
+      const sh = tileSize + distortionY
+
+      const dx = x * tileSize
+      const dy = y * tileSize
+      const dw = tileSize
+      const dh = tileSize
+
+      image(graphic, dx, dy, dw, dh, sx, sy, sw, sh)
+    }
+  }
+  
+  if (frameCount === 1) {
+    // saveCanvas('sketch)01', 'png');
+  }
+}
+
+
+function mousePressed(){
+  saveCanvas("sketch_01","png")
+}
